2c2bba64e37996c35afad2c05f29cb1c595b6cb9
Michael <berryintlonline@gmail.com>
2018-01-10 15:19:10 -0700

    Creating a file for environment variables

---
+++ env.sh
@@ -0,0 +1,12 @@
+home="~"
+
+# Dev Setup
+rootdevfolder="Desktop/Dev" # Your development directory
+devfolder="${home}${rootdevfolder}"
+
+# Personal info
+github="michaeljberry" # Your github username
+githubname="MichaelJBerry" # Your github username with proper capitalization
+fullname="Michael Berry"
+emailaddress="your email address"
+website="your website address"
